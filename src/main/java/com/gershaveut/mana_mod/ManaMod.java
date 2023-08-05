287960c7d6e17e45381a633fc52e575f84de507e
Владимир Николаев <vova1234567890012011@yandex.ru>
2023-08-05 18:19:51 +0300

    Completed TooltipItem, moved some items from ManaMod3

--- src/main/java/com/gershaveut/mana_mod/ManaMod.java
+++ src/main/java/com/gershaveut/mana_mod/ManaMod.java
@@ -3,17 +3,18 @@ package com.gershaveut.mana_mod;
 import com.gershaveut.mana_mod.world.item.CreativeModeTabs;
 import com.gershaveut.mana_mod.world.item.Items;
 import com.gershaveut.mana_mod.world.item.TooltipItem;
-import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
@@ -27,23 +28,24 @@ public class ManaMod {
         Log(Level.INFO, "Initializing");
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         CreativeModeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         Items.BLOCKS.register(modEventBus);
         Items.ITEMS.register(modEventBus);
-
         MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::registerBindings);
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    @SubscribeEvent
-    public void registerBindings(RegisterKeyMappingsEvent event) {
-        event.register(TooltipItem.keyTooltip);
-        event.register(TooltipItem.keyUsageItem);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
     }
 
     public static void Log(Level level, String message) {
         LOGGER.atLevel(level).log("[" + MODID + "] " + message);
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void registerBindings(RegisterKeyMappingsEvent event) {
+            event.register(TooltipItem.KEY_DESCRIPTION_ITEM);
+            event.register(TooltipItem.KEY_USAGE_ITEM);
+        }
     }
 }
--- src/main/java/com/gershaveut/mana_mod/world/item/Items.java
+++ src/main/java/com/gershaveut/mana_mod/world/item/Items.java
@@ -1,8 +1,10 @@
 package com.gershaveut.mana_mod.world.item;
 
 import com.gershaveut.mana_mod.ManaMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.FlintAndSteelItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +18,38 @@ public class Items {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    public static final RegistryObject<Item> MANA = ITEMS.register("mana", () -> new TooltipItem(new Item.Properties()));
-    public static final RegistryObject<Item> FLINT_AND_MANA = ITEMS.register("flint_and_mana", () -> new FlintAndSteelItem(new Item.Properties()));
+    public static final RegistryObject<Item> MANA = ITEMS.register("mana", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FLINT_AND_MANA = ITEMS.register("flint_and_mana", () -> new FlintAndSteelItem(new Item.Properties().durability(128)));
+    public static final RegistryObject<Item> MAGIC_LIGHTER = registerTooltipItem("magic_lighter", new Item.Properties().stacksTo(1), true, true, true);
+    public static final RegistryObject<Item> MANA_BAG = registerTooltipItem("mana_bag", new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON), true, false, true);
+    public static final RegistryObject<Item> MANA_FRIED = registerTooltipItem("mana_fried", new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(4).saturationMod(1.2F).build()), false, false, true);
+    public static final RegistryObject<Item> MANA_FUEL = registerTooltipItem("mana_fuel", new Item.Properties(), true, false, true);
+    public static final RegistryObject<Item> MANA_HEAT = registerTooltipItem("mana_heat", new Item.Properties().stacksTo(1).rarity(Rarity.RARE), false);
+    public static final RegistryObject<Item> MANA_INGOT = ITEMS.register("mana_ingot", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MANA_PIECE = ITEMS.register("mana_piece", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MANA_POTION = registerTooltipItem("mana_potion", new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).durability(3).food(new FoodProperties.Builder().alwaysEat().build()), true, false, true);
+    public static final RegistryObject<Item> MANA_SHARDS = ITEMS.register("mana_shards", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MANA_STAFF = registerTooltipItem("mana_staff", new Item.Properties().stacksTo(1), false, false, true);
+    public static final RegistryObject<Item> MANA_STICK = ITEMS.register("mana_stick", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MANA_STONE = registerTooltipItem("mana_stone", new Item.Properties().stacksTo(1), true, true, true);
+    public static final RegistryObject<Item> MANA_TALISMAN = registerTooltipItem("mana_talisman", new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON), true, true, true);
+    public static final RegistryObject<Item> SUPER_STONE = registerTooltipItem("super_stone", new Item.Properties().stacksTo(1).rarity(Rarity.RARE), true, true, true);
+    public static final RegistryObject<Item> UNSTABLE_MANA_PIECE = ITEMS.register("unstable_mana_piece", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> UNSTABLE_MANA_STICK = ITEMS.register("unstable_mana_stick", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> UNSTABLE_MANA_TALISMAN = registerTooltipItem("unstable_mana_talisman", new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON), true, true, true);
+
+    public static RegistryObject<Item> registerTooltipItem(String name, Item.Properties properties) {
+        return ITEMS.register(name, () -> new TooltipItem(properties, name));
+    }
+    public static RegistryObject<Item> registerTooltipItem(String name, Item.Properties properties, boolean descriptionItem) {
+        return ITEMS.register(name, () -> new TooltipItem(properties, name, descriptionItem));
+    }
+
+    public static RegistryObject<Item> registerTooltipItem(String name, Item.Properties properties, boolean descriptionItem, boolean UsageItem) {
+        return ITEMS.register(name, () -> new TooltipItem(properties, name, descriptionItem, UsageItem));
+    }
+
+    public static RegistryObject<Item> registerTooltipItem(String name, Item.Properties properties, boolean descriptionItem, boolean UsageItem, boolean WIP) {
+        return ITEMS.register(name, () -> new TooltipItem(properties, name, descriptionItem, UsageItem, WIP));
+    }
 }
