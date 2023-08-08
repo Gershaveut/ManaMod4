0d98dd64959a4c2dd32f53052d0e6064fda72b39
Gershaveut <vova1234567890012011@yandex.ru>
2023-08-08 22:22:38 +0300

    Working on ManaFried

--- src/main/java/com/gershaveut/mana_mod/network/protocol/game/ManaFriedPacket.java
+++ src/main/java/com/gershaveut/mana_mod/network/protocol/game/ManaFriedPacket.java
@@ -1,28 +1,24 @@
 package com.gershaveut.mana_mod.network.protocol.game;
 
 import com.gershaveut.mana_mod.network.protocol.MMPacket;
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
 
 public class ManaFriedPacket implements MMPacket {
-    public LivingEntity livingEntity;
-
-    public ManaFriedPacket(LivingEntity livingEntity) {
-        this.livingEntity = livingEntity;
+    public ManaFriedPacket() {
     }
 
-    public ManaFriedPacket(FriendlyByteBuf friendlyByteBuf) {
+    public ManaFriedPacket(FriendlyByteBuf buf) {
     }
 
     public static void handle(ManaFriedPacket message, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
-            message.livingEntity.heal(2F);
-            message.livingEntity.setArrowCount(0);
+            ServerPlayer sender = ctx.get().getSender();
+            assert sender != null;
+            sender.setRespawnPosition(sender.getCommandSenderWorld().dimension(), sender.getOnPos(), sender.getRespawnAngle(), true, true);
         });
         ctx.get().setPacketHandled(true);
     }
--- src/main/java/com/gershaveut/mana_mod/world/item/ManaFried.java
+++ src/main/java/com/gershaveut/mana_mod/world/item/ManaFried.java
@@ -2,12 +2,10 @@ package com.gershaveut.mana_mod.world.item;
 
 import com.gershaveut.mana_mod.network.protocol.MMPacketHandler;
 import com.gershaveut.mana_mod.network.protocol.game.ManaFriedPacket;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.network.PacketDistributor;
 
 public class ManaFried extends Item {
     public ManaFried(Properties properties) {
@@ -16,8 +14,7 @@ public class ManaFried extends Item {
 
     @Override
     public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
-        if (livingEntity instanceof ServerPlayer player)
-            MMPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new ManaFriedPacket(livingEntity));
+        MMPacketHandler.INSTANCE.sendToServer(new ManaFriedPacket());
         return super.finishUsingItem(itemStack, level, livingEntity);
     }
 }
