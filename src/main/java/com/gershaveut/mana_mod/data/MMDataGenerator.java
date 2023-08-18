211815808141c3846ea81002bff8efdb34bd10e1
Gershaveut <vova1234567890012011@yandex.ru>
2023-08-18 13:05:33 +0300

    Adding Mana Ore Generation

--- src/main/java/com/gershaveut/mana_mod/data/MMDataGenerator.java
+++ src/main/java/com/gershaveut/mana_mod/data/MMDataGenerator.java
@@ -1,21 +1,36 @@
 package com.gershaveut.mana_mod.data;
 
 import com.gershaveut.mana_mod.data.recipes.packs.MMRecipeProvider;
+import com.gershaveut.mana_mod.data.worldgen.MMWorldGenProvider;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.concurrent.CompletableFuture;
+
 import static com.gershaveut.mana_mod.ManaMod.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MMDataGenerator {
+    private static DataGenerator dataGenerator;
+    private static boolean includeServer;
+    
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
-        DataGenerator dataGenerator = event.getGenerator();
+        dataGenerator = event.getGenerator();
+        includeServer = event.includeServer();
         PackOutput packOutput = event.getGenerator().getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         
-        dataGenerator.addProvider(event.includeServer(), new MMRecipeProvider(packOutput));
+        addProvider(new MMRecipeProvider(packOutput));
+        addProvider(new MMWorldGenProvider(packOutput, lookupProvider));
+    }
+    
+    private static <T extends DataProvider> void addProvider(T provider) {
+        dataGenerator.addProvider(includeServer, provider);
     }
 }
