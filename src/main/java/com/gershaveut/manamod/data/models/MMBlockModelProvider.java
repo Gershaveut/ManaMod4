c21a7b63ec170dc3ee5736ea4e3c82490f5fb23b
Владимир Николаев <vova1234567890012011@yandex.ru>
2023-08-19 18:10:55 +0300

    Work on generating block models

--- src/main/java/com/gershaveut/manamod/data/models/MMBlockModelProvider.java
+++ src/main/java/com/gershaveut/manamod/data/models/MMBlockModelProvider.java
@@ -1,9 +1,18 @@
 package com.gershaveut.manamod.data.models;
 
 import com.gershaveut.manamod.ManaMod;
+import com.gershaveut.manamod.world.level.block.MMBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockModelProvider;
+import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Objects;
 
 public class MMBlockModelProvider extends BlockModelProvider {
     public MMBlockModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -12,6 +21,28 @@ public class MMBlockModelProvider extends BlockModelProvider {
 
     @Override
     protected void registerModels() {
+        basicBlock(MMBlocks.MANA_BLOCK.get());
+        basicBlock(MMBlocks.MANA_BRICKS.get());
+        basicBlock(MMBlocks.CHISELED_MANA_BRICKS.get());
+        basicBlock(MMBlocks.MANA_BRICK_STAIRS.get(), MMBlocks.MANA_BRICKS.get());
+        basicBlock(MMBlocks.MANA_BRICK_WALL.get(), MMBlocks.MANA_BRICKS.get());
+        basicBlock(MMBlocks.MANA_BRICK_SLAB.get(), MMBlocks.MANA_BRICKS.get());
+        //basicBlock(MMBlocks.MANA_CAKE.get());
+        basicBlock(MMBlocks.MANA_ORE.get());
+        basicBlock(MMBlocks.MANA_SHIELD.get());
+        basicBlock(MMBlocks.MANA_SYNTHESIZER.get());
+        basicBlock(MMBlocks.UNSTABLE_MANA_BLOCK.get());
+    }
 
+    private BlockModelBuilder basicBlock(Block block) {
+        return cubeAll(blockPrefix(block).getPath(), ManaMod.prefix("block/" + blockPrefix(block).getPath()));
+    }
+
+    private BlockModelBuilder basicBlock(Block block, Block foreign) {
+        return cubeAll(blockPrefix(block).getPath(), ManaMod.prefix("block/" + blockPrefix(foreign).getPath()));
+    }
+
+    private ResourceLocation blockPrefix(Block block) {
+        return Objects.requireNonNull(ForgeRegistries.BLOCKS.getKey(block));
     }
 }
