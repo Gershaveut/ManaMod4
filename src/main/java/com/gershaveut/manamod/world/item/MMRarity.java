30e763beaa6a14c85a79fdf5bbac43156e181978
Gershaveut <vova1234567890012011@yandex.ru>
2023-10-10 20:42:05 +0300

    Added mana font

--- src/main/java/com/gershaveut/manamod/world/item/MMRarity.java
+++ src/main/java/com/gershaveut/manamod/world/item/MMRarity.java
@@ -1,8 +1,9 @@
 package com.gershaveut.manamod.world.item;
 
-import com.gershaveut.manamod.ManaMod;
+import net.minecraft.ChatFormatting;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Rarity;
 
 public class MMRarity {
-    public static final Rarity MANA = Rarity.create("mana", style -> style.withFont(ManaMod.prefix("default")));
+    public static final Rarity MANA = Rarity.create("mana", style -> style.withFont(new ResourceLocation("minecraft", "mana")).withColor(ChatFormatting.AQUA));
 }
